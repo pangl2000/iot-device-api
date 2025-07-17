33a4fef1a8c5f62962aec6f1c5cc367b7b2b200a
Pannic <pangl2000@gmail.com>
2025-07-17 15:43:45 +0300

    Initial Commit

---
+++ src/main/java/com/example/iot_device_api/IotDeviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.iot_device_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDeviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDeviceApiApplication.class, args);
+	}
+
+}
